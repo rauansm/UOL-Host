05aa18d9b83e1d50c176fe1e3937cde991975b02
Rauan Santos <rauanmoreira259@gmail.com>
2024-02-16 15:08:34 -0300

    inicia projeto

---
+++ src/main/java/br/com/uol/UOL/HOST/UolHostApplication.java
@@ -0,0 +1,13 @@
+package br.com.uol.UOL.HOST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolHostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolHostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uol/UOL/HOST/UolHostApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uol.UOL.HOST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
